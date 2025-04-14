1ec0401dbd7408be6487a539b2054a7c31d13edc
Frankiga-GA <mguistv@.com>
2025-04-14 14:23:18 -0500

    Estructura del Prouecto Terminado

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+//Motor de plantillas 
+app.set('view engine', 'ejs');
+
+
+app.listen(5000, () => {
+  console.log("Servidor ejecutandose en http://localhost:5000")
+});
